808483d0d27b5c2686573b5ed325fd4e5bdf9ad7
andrey <a@gmail.com>
2026-09-21 23:29:08 +0300

    Fix 6 and 7

--- 6.py
+++ 6.py
@@ -3,8 +3,9 @@ maxi : int = -301
 num : int = 0
 
 for i in range(len(D) - 2):
-    if int(D[i]) + int(D[i + 1]) + int(D[i + 2]) > maxi:
-        maxi = int(D[i])
+    m : int = int(D[i]) + int(D[i + 1]) + int(D[i + 2])
+    if m > maxi:
+        maxi = m
         num = i
 
 print(num, num + 1, num + 2)
--- 7.py
+++ 7.py
@@ -1,16 +1,12 @@
 import math
 D : list[str]= input().split()
-maximum : int = -201
+maximum : float = -201.
 num : int = 0
 
 for i in range(len(D) - 1):
-    if int(D[i]) > int(D[i + 1]):
-        if  math.fabs(int(D[i]) - int(D[i + 1])) > maximum:
-            maximum = int(D[i])
-            num = i
+    m : float = math.fabs(int(D[i]) - int(D[i + 1]))
+    if m > maximum:
+        maximum = m
+        num = i
 
-    else:
-        if math.fabs(int(D[i + 1]) - int(D[i])) > maximum:
-            maximum = int(D[i])
-            num = i
 print(num, num + 1)
